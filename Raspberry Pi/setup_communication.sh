cb4524973fe3aae482557c43b5f36c9a85507668
simon.thiel2+github@gmail.com <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>
2022-06-30 11:16:32 +0200

    added -y for silent

--- Raspberry Pi/setup_communication.sh
+++ Raspberry Pi/setup_communication.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo adduser garduino dialout
-sudo apt-get install python3
-sudo apt-get install python3-pip
+sudo apt-get install -y python3
+sudo apt-get install -y python3-pip
 
 python3 -m pip install pyserial
 python3 -m pip install influxdb
