3b22d28c4d5282865ee361ca7825cab78c04fce5
Simon Thiel <simonthiel2@gmail.com>
2022-07-28 16:07:31 +0200

    changing to installing from requirements.txt

--- Raspberry Pi/setup_communication.sh
+++ Raspberry Pi/setup_communication.sh
@@ -4,6 +4,4 @@ sudo adduser garduino dialout
 sudo apt-get install -y python3
 sudo apt-get install -y python3-pip
 
-python3 -m pip install pyserial
-python3 -m pip install influxdb
-python3 -m pip install beautifulsoup4
+python3 -m pip install -r requirements.txt
